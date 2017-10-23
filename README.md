b04f66042019d941886bd16277772e02e57fe8bf
joelsmith <joelhsmth@gmail.com>
2017-10-23 17:19:58 -0500

    added dfuse simulations

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # tractlengths
-Scripts to simulate haplotypes using [forqs](https://bitbucket.org/dkessner/forqs) and 
-plot ancestry tract length distributions.
+Scripts to simulate haplotypes using [forqs](https://bitbucket.org/dkessner/forqs) or 
+[dfuse](http://www.uwyo.edu/buerkle/software/dfuse) and compare ancestry tract length
+distributions to model predictions from [Gravel (2012)](https://github.com/sgravel/tracts).
--- run_tractlengths.py
+++ run_tractlengths.py
@@ -1,21 +1,39 @@
 
 %reset
 %matplotlib
-#----------parameters----------
-popsize = 2000
+
+#-----neutral parameters-------
+popsize = 1000
 chrlen = 1000000
-ratevec = [.001,0.03,0.05]
-tstart = 30 
-npts = 20 
-maxlen = 1 
-pop = 1 
+markers = 1000
+ratevec = [.001,.03,.05]
+tstart = 30
+npts = 20
+maxlen = 1
+pop = 1
 Ls = [1]
+migscheme = 'hybrid'
+#------selection parameters-----
+dmipos = (.3,.7)
+seldmi = .9
+selanc = 0
+domdmi = 0
 #-------------------------------
 
 exec(open("/home/joelsmith/Projects/dmis/code/tractlengths/forqs_tractlengths.py").read())
+exec(open("/home/joelsmith/Projects/dmis/code/tractlengths/dfuse_tractlengths.py").read())
 
-run_forqs(ratevec, tstart, popsize, chrlen)
-get_tracts(ratevec, chrlen, maxlen, npts)
-plot_figure(ratevec, tstart, Ls, pop, maxlen, npts)
+#----------------dmi tract length distributions-------------------
+run_dfuse_dmis(ratevec, tstart, popsize, markers, dmipos, seldmi, selanc, domdmi)
+get_dfuse_tracts(ratevec,npts,maxlen,chrlen)
+plot_figure_dfuse(ratevec, tstart, Ls, pop, maxlen, npts, migscheme)
 
+#-----------neutral tract length distributions-------------------
+run_dfuse(ratevec, tstart, popsize, markers)
+get_dfuse_tracts(ratevec,npts,maxlen,chrlen)
+plot_figure_dfuse(ratevec, tstart, Ls, pop, maxlen, npts, migscheme)
 
+run_forqs(ratevec, tstart, popsize, chrlen, migscheme)
+get_forqs_tracts(ratevec, chrlen, maxlen, npts, migscheme)
+plot_figure_forqs(ratevec, tstart, Ls, pop, maxlen, npts, migscheme)
+#----------------------------------------------------------------
